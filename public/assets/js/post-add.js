01548cb06c3abf1618704ee5408525f6dee72b6e
annymei <anny1541@163.com>
2019-08-12 21:54:19 +0800

    更新第三次啦

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -27,14 +27,17 @@ $('#feature').on('change', function () {
     //发送ajax
     $.ajax({
         type: 'post',
-		url: '/posts',
-		data: formData,
+        url: '/upload',
+        data: formData,
         //告诉ajax方法不要处理data属性对应的参数
         processData: false,
         //告诉ajax不要设置参数类型
         contentType: false,
         success: function (res) {
+            // console.log(res);
+            
             $('#thumbnail').val(res[0].cover);
+            $('#prev').show().attr('src', res[0].cover);
         }
     })
 })
@@ -52,7 +55,7 @@ $('#addForm').on('submit', function () {
         data: formData,
         success: function () {
             console.log(123456);
-            
+
             // 成功之后重定向到文章列表页面
             location.href = '/admin/posts.html'
 
@@ -61,3 +64,100 @@ $('#addForm').on('submit', function () {
     //阻止表单的默认行为
     return false;
 })
+
+//根据是否有id判断是添加文章还是修改文章
+
+//获取浏览器地址栏中的id参数
+var id = getUrl('id');
+//当管理员是在修改文章
+if (id != -1) {
+    //发送ajax 获得在这篇文章的相关信息
+    $.ajax({
+        type: 'get',
+        url: '/posts/' + id,
+        success: function (res) {
+            console.log(res);
+            $('#addPost').hide();
+            $('#editPost').show();
+            $('h1').html('修改文章')
+         
+            //获取res返回的文章信息中的内容显示在表单中
+            $('#title').val(res.title);
+            $('#content').val(res.content);
+            //需要加此自定义属性才可以截取时间
+            $('#created').attr('type', 'date');
+            // console.log(res.createAt.split('T'));
+            //截取事件格式后返回的是数组,我们只需要年月日即第一个元素[0]
+            $('#created').val(res.createAt.split('T')[0]);
+
+            //获取#category下面所有的option标签 所属分类
+            let coption = $('#category > option');
+            //  console.log(coption);
+            //循环option
+            coption.each(function (index, item) {
+                // console.log(item); //是所有的分类及其id
+
+                //将item这个对象转换为jQuery
+                // console.log($(item).attr("value"),res.category);
+                //将每一个循环出来的id和res回来的id进行对比,如果相等则选中改数组即item
+                if ($(item).attr("value") == res.category) {
+                    $(item).prop('selected', true);
+                };
+            });
+            // 获取status下面所有的option标签 为状态筛选
+            let soption = $('#status> option');
+            // console.log(soption);
+            soption.each(function(index,item){
+                // console.log(item);//是所有的分类及其id
+                // console.log($(item).attr("value") ,res.state);
+                
+                if($(item).attr("value") == res.state){
+                    $(item).prop('selected',true);
+                }        
+            })
+
+
+           //将返回的图片格式传入隐藏域
+            $('#thumbnail').val(res.thumbnail);
+            $('#prev').show().attr('src', res.thumbnail);
+
+        }
+    })
+}
+
+//编辑注册点击事件
+$('#editPost').on('click',function(){
+      //获取表单中输入的内容
+      let msg = $('#addForm').serialize();
+    //   console.log(msg);
+    // 发送ajax请求数据
+    $.ajax({
+        type:'put',
+        url:'/posts/'+id,
+        data:msg,
+        success:function(){
+            location.href = '/admin/posts.html';
+        }
+    })
+      
+})
+
+
+
+
+//从浏览器的地址栏中获取参数
+function getUrl(name) {
+    //  console.log( location.search.substr(1).split('&'));  //查询的是浏览器中的id
+    let parmData = location.search.substr(1).split('&');
+
+    //循环parmData数组
+    for (var i = 0; i < parmData.length; i++) {
+        let tem = parmData[i].split('=');
+        if (tem[0] == name) {
+            return tem[1]
+        }
+    }
+    return -1;
+}
+
+
--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,24 +1,45 @@
-//方ajax获得文章列表数据
-$.ajax({
-    type: 'get',
-    url: '/posts',
-    success: function (res) {
-        // console.log(res);
 
-        //res返回的是一个数组,是所有的信息集合
-        let html = template('poPid', res);
-        // console.log(html);
-        $('#postBox').html(html);
-        //翻页
-        let page = template('pagePid', res);
-        // console.log(html);
 
-        //放入ul中
-        $('#page').html(page)
 
-    }
+//没有传参则显示所有的 分类和所有的状态
+render();
 
-});
+//封装函数
+function render(c = "all", s = "all", page = 1) {
+    // console.log(123456);
+
+    //方ajax获得文章列表数据
+    $.ajax({
+        type: 'get',
+        url: '/posts',
+        data: {
+            page: page, //页码
+            category: c, //分类名称
+            state: s   //分类状态
+        },
+        success: function (res) {
+
+            // console.log(res);
+            //获取当前页码,并开放到全局当中,这样全局都可以拿到此参数
+            window.currentPage = res.page
+            // console.log(currentPage);
+
+            //res返回的是一个数组,是所有的信息集合
+            let html = template('poPid', res);
+            // console.log(html);
+            $('#postBox').html(html);
+            //翻页
+            let page = template('pagePid', res);
+            // console.log(html);
+
+            //放入ul中
+            $('#page').html(page)
+
+        }
+
+    });
+}
+
 
 //处理时间格式
 function formDate(date) {
@@ -29,28 +50,69 @@ function formDate(date) {
 }
 
 //当点击分页时就触发函数
-function changePage() {
-    //发送ajax请求  获取文章列表数据
-    $.ajax({
-        type: 'get',
-        url: '/posts',
-        // page:'5',
-        // size:'5',
-        data: {
-            page: page
-        },
-        success: function (res) {
-            console.log(res);
-            //res返回的是一个数组,是所有的信息集合
-            let html = template('poPid', res);
-            // console.log(html);
-            $('#postBox').html(html);
-            // 将信息集合放入模板中
-            let page = template('pagePid', res);
-            // console.log(html);
+function changePage(page) {
 
-            //放入ul中
-            $('#page').html(page)
-        }
-    })
+    let s = $('#s').val();
+    let c = $('#categoryBox').val();
+    render(c, s, page)
+
 }
+
+//筛选分类
+
+
+$.ajax({
+    type: 'get',
+    url: '/categories',
+    success: function (res) {
+        // console.log(res);
+        //拼接模板
+        let html = template('sShearch', { data: res });
+        // console.log(html);
+        //将拼接好的字符串放入select选项卡中
+        $('#categoryBox').append(html);
+    }
+});
+
+//给筛选按钮绑定点击事件
+$('#sBtn').on('click', function () {
+    //获取筛选条件 获取到分类id与状态
+    let s = $('#s').val();
+    let c = $('#categoryBox').val();
+    render(c, s);
+})
+
+var delId;
+//根据id删除 通过事件委托 绑定事件
+$('#postBox').on('click', '.del', function () {
+    // console.log(123456);
+    //确认是否删除
+    if (confirm('您确定要删除嘛')) {
+        //获取id
+        delId = $(this).attr('data-id');
+        // console.log(delId);
+
+        //发送ajax
+        $.ajax({
+            type: 'delete',
+            url: '/posts/' + delId,
+            success: function (res) {
+                console.log(res);
+                //如果tbody标签下面的有标签 这个时候我们就让它在当前页码如果已经 已经大于1 我们应该让它回到前一页
+                if ($('tbody').children().length > 1) {
+                    let s = $('#s').val();
+                    let c = $('#categoryBox').val();
+                    // 如果当前页码已经是第一页了 我们就让他不跳转到前一页
+                    if (currentPage == 1) {
+                        render(c, s, currentPage);
+                    } else {
+                        render(c, s, currentPage - 1)
+                    }
+                } else {
+                    render(c, s, currentPage - 1);
+                }
+
+            }
+        });
+    }
+})
